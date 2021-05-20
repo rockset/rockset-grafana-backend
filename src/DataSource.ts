20f88021b9ec0d15a1344a71e1f71a864aff23a7
Jeff Dunham <jeff@rockset.com>
2021-05-20 12:45:14 -0700

    Adding support for grafana template variables

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -1,9 +1,17 @@
 import { DataSourceInstanceSettings } from '@grafana/data';
 import { DataSourceWithBackend } from '@grafana/runtime';
 import { RocksetDataSourceOptions, RocksetQuery } from './types';
+import { getTemplateSrv } from '@grafana/runtime';
 
 export class DataSource extends DataSourceWithBackend<RocksetQuery, RocksetDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<RocksetDataSourceOptions>) {
     super(instanceSettings);
+  }
+  applyTemplateVariables(query: RocksetQuery) {
+    const templateSrv = getTemplateSrv();
+    return {
+      ...query,
+      queryText: query.queryText ? templateSrv.replace(query.queryText) : '',
+    };
   }
 }
